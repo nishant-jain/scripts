30009a45fef234c0a70e6e1e1cb074a1dd077092
Nishant Jain <nishantjain19991@gmail.com>
2013-06-21 02:09:13 +0530

    second commit

---
+++ python/scrapy/xkcd/xkcd/spiders/xkcd_spider.py
@@ -0,0 +1,23 @@
+import re
+
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from xkcd.items import XkcdItem
+
+class XkcdSpiderSpider(CrawlSpider):
+    name = 'xkcd_spider'
+    allowed_domains = ['xkcd.com']
+    start_urls = ['http://www.xkcd.com/']
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = XkcdItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
