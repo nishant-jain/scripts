bf40a3698a84758f096d0d274197e7ea29b290f9
Nishant <nishantjain19991@gmail.com>
2014-04-18 22:40:16 +0530

    Fixed

--- bash/xkcd.sh
+++ bash/xkcd.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-#script to get the latest xkcd comic. Worked in ubuntu 10.(I guess.) 
+#script to get the latest xkcd comic. 
 wget http://www.xkcd.com
-sleep 5
-b=`cat index.html | egrep "png</h3>" | awk '{print $5}' | awk -F '<' '{print $1}'`
-wget $b
-c=`cat index.html | egrep "png</h3>" | awk '{print $5}' | awk -F '<' '{print $1}' | awk -F '/' '{print $5}'`
-eog $c
+eog ` cat index.html | egrep "Image URL" | awk '{print $5}' `
 rm ./index.html
-rm $c
+
 
